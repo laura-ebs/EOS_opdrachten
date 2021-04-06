cb282bf244ef87e543e9549b6db1bb706408da1f
laura-ebs <74971564+laura-ebs@users.noreply.github.com>
2021-04-06 14:12:10 +0200

    Update yeet.cpp

--- yeet.cpp
+++ yeet.cpp
@@ -1,4 +1,5 @@
-#include<wiringPi.h>
+
+		#include<wiringPi.h>
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -6,7 +7,7 @@ using namespace std;
 int main(void){
 
 //set de pinnen
-int paars=15;
+int paars=29;
 int groen=16;
 int wit=1;
 int blauw=10;
@@ -30,6 +31,13 @@ bool bluethen=LOW;
 bool redthen=LOW;
 bool yellowthen=LOW;
 
+bool purplenow=LOW;
+bool greennow=LOW;
+bool whitenow=LOW;
+bool bluenow=LOW;
+bool rednow=LOW;
+bool yellownow=LOW;
+
 //vector om kleuren in op te slaan
 vector<int> kleuren={};
 
@@ -37,46 +45,51 @@ vector<int> kleuren={};
 for (;;){
 	
 cout<<"Druk de knop in voor de eerste kleur: \n";
+delay(4000);
 
 //lezen input van knop
-bool purplenow=digitalRead(paars);
-bool greennow=digitalRead(groen);
-bool whitenow=digitalRead(wit);
-bool bluenow=digitalRead(blauw);
-bool rednow=digitalRead(rood);
-bool yellownow=digitalRead(geel);
-
-delay(6000);
+purplenow=digitalRead(paars);
+greennow=digitalRead(groen);
+whitenow=digitalRead(wit);
+bluenow=digitalRead(blauw);
+rednow=digitalRead(rood);
+yellownow=digitalRead(geel);
 
 	if(purplethen != purplenow){
 		if(purplenow==HIGH){
 			kleuren.push_back(5);
+			purplenow=LOW;
 			
 		}
 	}
 	if(greenthen != greennow){
 		if(greennow==HIGH){
 			kleuren.push_back(1);
+			greennow=LOW;
 		}
 	}
 	if(whitethen != whitenow){
 		if(whitenow==HIGH){
 			kleuren.push_back(4);
+			whitenow=LOW;
 		}
 	}
 	if(bluethen != bluenow){
 		if (bluenow==HIGH){
 			kleuren.push_back(0);
+			bluenow=LOW;
 			}
 	}
 	if(redthen != rednow){
 		if(rednow==HIGH){
 			kleuren.push_back(2);
+			rednow=LOW;
 		}
 	}
 	if(yellowthen != yellownow){
 		if (yellownow==HIGH){
 			kleuren.push_back(3);
+			yellownow=LOW;
 		}
 	}
 	purplethen = purplenow;
@@ -93,16 +106,15 @@ delay(6000);
 for (;;){
 	
 cout<<"Druk de knop in voor de tweede kleur: \n";
+delay(4000);
 
 //lezen input van knop
-bool purplenow=digitalRead(paars);
-bool greennow=digitalRead(groen);
-bool whitenow=digitalRead(wit);
-bool bluenow=digitalRead(blauw);
-bool rednow=digitalRead(rood);
-bool yellownow=digitalRead(geel);
-
-delay(6000);
+purplenow=digitalRead(paars);
+greennow=digitalRead(groen);
+whitenow=digitalRead(wit);
+bluenow=digitalRead(blauw);
+rednow=digitalRead(rood);
+yellownow=digitalRead(geel);
 
 	if(purplethen != purplenow){
 		if(purplenow==HIGH){
@@ -142,8 +154,119 @@ delay(6000);
 	redthen = rednow;
 	yellowthen = yellownow;
 	if(kleuren.size()==2){
+	 break;
+	}
+}
+
+for (;;){
+	
+cout<<"Druk de knop in voor de derde kleur: \n";
+delay(4000);
+
+//lezen input van knop
+bool purplenow=digitalRead(paars);
+bool greennow=digitalRead(groen);
+bool whitenow=digitalRead(wit);
+bool bluenow=digitalRead(blauw);
+bool rednow=digitalRead(rood);
+bool yellownow=digitalRead(geel);
+
+	if(purplethen != purplenow){
+		if(purplenow==HIGH){
+			kleuren.push_back(5);	
+		}
+	}
+	if(greenthen != greennow){
+		if(greennow==HIGH){
+			kleuren.push_back(1);
+		}
+	}
+	if(whitethen != whitenow){
+		if(whitenow==HIGH){
+			kleuren.push_back(4);
+		}
+	}
+	if(bluethen != bluenow){
+		if (bluenow==HIGH){
+			kleuren.push_back(0);
+			}
+	}
+	if(redthen != rednow){
+		if(rednow==HIGH){
+			kleuren.push_back(2);
+		}
+	}
+	if(yellowthen != yellownow){
+		if (yellownow==HIGH){
+			kleuren.push_back(3);
+		}
+	}
+	purplethen = purplenow;
+	greenthen = greennow;
+	whitethen = whitenow;
+	bluethen = bluenow;
+	redthen = rednow;
+	yellowthen = yellownow;
+	if(kleuren.size()==3){
 		
-	 cout<<"De ander speler moet nu uw code raden \n";
+	 break;
+	}
+}
+
+for (;;){
+	
+cout<<"Druk de knop in voor de laaste kleur: \n";
+delay(4000);
+
+//lezen input van knop
+bool purplenow=digitalRead(paars);
+bool greennow=digitalRead(groen);
+bool whitenow=digitalRead(wit);
+bool bluenow=digitalRead(blauw);
+bool rednow=digitalRead(rood);
+bool yellownow=digitalRead(geel);
+
+
+	if(purplethen != purplenow){
+		if(purplenow==HIGH){
+			kleuren.push_back(5);
+			
+		}
+	}
+	if(greenthen != greennow){
+		if(greennow==HIGH){
+			kleuren.push_back(1);
+		}
+	}
+	if(whitethen != whitenow){
+		if(whitenow==HIGH){
+			kleuren.push_back(4);
+		}
+	}
+	if(bluethen != bluenow){
+		if (bluenow==HIGH){
+			kleuren.push_back(0);
+			}
+	}
+	if(redthen != rednow){
+		if(rednow==HIGH){
+			kleuren.push_back(2);
+		}
+	}
+	if(yellowthen != yellownow){
+		if (yellownow==HIGH){
+			kleuren.push_back(3);
+		}
+	}
+	purplethen = purplenow;
+	greenthen = greennow;
+	whitethen = whitenow;
+	bluethen = bluenow;
+	redthen = rednow;
+	yellowthen = yellownow;
+	if(kleuren.size()==4){
+		
+	 cout<<"De ander speler moet nu uw kleurcode raden \n";
 	 break;
 	}
 }
@@ -154,4 +277,3 @@ for(unsigned int i = 0; i < kleuren.size(); i++){
 	}
 	cout<<"\n";
 }
-
